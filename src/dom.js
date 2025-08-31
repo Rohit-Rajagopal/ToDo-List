be759314db1764b6dd3cbc3b27c9066c60a3cc09
Rohit <rohitrajagopal04@gmail.com>
2025-08-31 18:04:28 +0530

    added new project functionality

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,42 @@
-const DomManager = (function() {
-    const content = document.querySelector('#content');
-    
-})
+class DomManager {
+    content = document.querySelector('#content');
+    projectSelector = document.querySelector('#project-selector');
+    newProjDialog = document.querySelector('.newProjectForm');
+    newProjConfirm = document.querySelector('.confirm-proj-name');
+    newProjButton = document.querySelector('.create-proj')
+
+    constructor() {
+        this.newProjButton.addEventListener('click', () => {this.newProjForm()})
+    }
+
+    showProject(project) {
+        this.content.innerHTML = ''
+        
+        const h2 = document.createElement('h2');
+        h2.textContent = `${project.title}`
+        const ol = document.createElement('ol');
+        for (let item of project.toDoList) {
+            let li = document.createElement('li');
+            li.textContent = `${item.title}  Due Date:${item.dueDate}`;
+            ol.appendChild(li);
+        }
+        this.content.appendChild(h2);
+        this.content.appendChild(ol);
+    }
+
+    addProject(projectTitle) {
+        const option = document.createElement('option');
+        option.value = projectTitle;
+        option.textContent = projectTitle;
+        this.projectSelector.appendChild(option);
+    }
+
+    newProjForm() {
+        this.newProjDialog.showModal();
+        this.newProjConfirm.addEventListener('click', () => {
+            this.newProjDialog.close()
+        })
+    }
+}
+
+export {DomManager};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import {Project, ToDoItem} from './project.js';
+import { DomManager } from './dom.js';
 
 
 class Manager {
-    projects = [];
+    projects = {};
+    domManager = new DomManager();
+    curProj = null;
+    projectSelector = document.querySelector('#project-selector')
+    newProjName = document.querySelector('.proj-name');
+    newProjConfirm = document.querySelector('.confirm-proj-name');
 
     constructor() {
-        this.projects.push(new Project('default'));
+        this.projects['Default'] = new Project('Default');
+        this.domManager.addProject('Default');
+        this.domManager.showProject(this.projects['Default'])
+        this.curProj = 'Default';
+        this.projectSelector.addEventListener('change', (e) => {
+            this.domManager.showProject(this.projects[this.projectSelector.value]);
+            this.curProj = this.projectSelector.value;
+        });
+        this.newProjConfirm.addEventListener('click', () => {this.createNewProject(this.newProjName.value)});
+    }
+
+    createNewProject(title) {
+        const newProj = new Project(title);
+        this.projects[title] = newProj;
+        this.domManager.addProject(title);
     }
 }
+
+const manager = new Manager();
